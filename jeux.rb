5282e4388c9b40b729694de8d2ac7a8fe69565dd
Nicolas <nicolas111boivin@outlook.fr>
2020-01-24 22:04:36 +0100

    first commit

---
+++ jeux.rb
@@ -0,0 +1,15 @@
+puts "bonjour pour joue a se peti jeux ses tré sinple
+il sufi de soisire un ame
+1 epe 2 renade"
+puts "vour erte pre"
+oui = gets
+vie = "30"
+if oui  == oui
+  puts " ses parti"
+end
+ame = gets
+if ame=="1"
+  vie2 = vie - 5
+
+end
+puts vie2
